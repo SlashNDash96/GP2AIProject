0456efca307daeffdfb539e589ad91d033ec2ebe
Jayden Martinez <145072355+SlashNDash96@users.noreply.github.com>
2025-01-08 11:44:30 -0800

    Waypoints Continued
    Added more to the scripts.

--- AIProjectMartinez/Assets/Scripts/FollowWaypoint.cs
+++ AIProjectMartinez/Assets/Scripts/FollowWaypoint.cs
@@ -9,22 +9,38 @@ public class FollowWaypoint : MonoBehaviour
     int currentWP = 0;
 
     public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
+
+    GameObject tracker;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker,GetComponent<Collider>());
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
+
     }
+    void ProgressTracker()
+    {
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) > 3)
+            currentWP++;
+        if (currentWP >= waypoints.Length)
+            currentWP = 0;
+
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, 0.1f);
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
-            currentWP++;
+        ProgressTracker();
 
-        if (currentWP >= waypoints.Length)
-            currentWP = 0;
+        //Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        //this.transform.rotation = Quaternion.Slerp(transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
 
-        this.transform.LookAt(waypoints[currentWP].transform);
-        this.transform.Translate(0, 0, speed *  Time.deltaTime);
+        //this.transform.Translate(0, 0, speed *  Time.deltaTime);
     }
 }
