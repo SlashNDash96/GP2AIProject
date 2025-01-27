e232529f905fb09014d85317ea5992270a2dc666
Jayden Martinez <145072355+SlashNDash96@users.noreply.github.com>
2025-01-27 12:00:42 -0800

    Finite State Machines
    started FSM

---
+++ AIProjectMartinez/Assets/Scripts/State.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class State : MonoBehaviour
+{
+    public enum STATE
+    {
+        IDLE, PATROL, PURSUE, ATTACK, SLEEP
+    };
+
+    public enum EVENT
+    {
+        ENTER, UPDATE, EXIT
+    };
+
+    public STATE name;
+    protected EVENT stage;
+    protected GameObject npc;
+    protected Animator anim;
+    protected Transform player;
+    protected State nextState;
+
+    float visDist = 10.0f;
+    float visAngle = 30.0f;
+    float shootDist = 7.0f;
+
+    public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+    {
+        npc = _npc;
+        
+    }
+}
